b9eb419376cb5bd763f36987154516bc33c8c152
Parthajeet-Sarmah <coderparth32768@gmail.com>
2026-09-20 10:09:38 +0530

    input wait, build script updated

--- src/Application/BareLLMServer/BareLLMServer.c
+++ src/Application/BareLLMServer/BareLLMServer.c
@@ -4,14 +4,22 @@
 #include <Library/DebugLib.h>
 #include <Library/PcdLib.h>
 
+#include <Library/UefiBootServicesTableLib.h>
+
 EFI_STATUS
 EFIAPI
 UefiMain (IN EFI_HANDLE ImageHandle, IN EFI_SYSTEM_TABLE *SystemTable) {
+	gST->ConOut->ClearScreen(gST->ConOut);
 	Print(L"BareLLM UEFI Server v0.1\r\n");
 	Print(L"Booted successfully.\r\n");
 	UINT32 Index = 0;
 	for (Index=0; Index<FixedPcdGet64(PcdBareLLMMaxConnections); Index++) {
 		Print(L"Hello World\r\n");
 	}
+
+	gBS->WaitForEvent(1, &gST->ConIn->WaitForKey, NULL);
+
+	EFI_INPUT_KEY Key;
+	gST->ConIn->ReadKeyStroke(gST->ConIn, &Key);
 	return EFI_SUCCESS;
 }
--- src/build.sh
+++ src/build.sh
@@ -1,19 +1,65 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -e
+
+# --------------------------------------------------
+# Paths
+# --------------------------------------------------
+
+export WORKSPACE="/home/crankshaft/Projects/baremetallm/edk/edk2"
+export PACKAGES_PATH="/home/crankshaft/Projects/baremetallm/src"
+
+EDK2="$WORKSPACE"
+SRC="/home/crankshaft/Projects/baremetallm/src"
+
+OVMF_CODE="/usr/share/edk2/x64/OVMF_CODE.4m.fd"
+OVMF_VARS="/usr/share/edk2/x64/OVMF_VARS.4m.fd"
+
+BUILD_DIR="$SRC/Build/BareLLMServer/DEBUG_GCC/X64"
+ESP="/tmp/esp"
+VARS="/tmp/OVMF_VARS.4m.fd"
+
+# --------------------------------------------------
+# Setup EDK II
+# --------------------------------------------------
+
+cd "$EDK2"
+
+source "$EDK2/edksetup.sh"
+
+# --------------------------------------------------
+# Build
+# --------------------------------------------------
 
 build \
-      -p /home/crankshaft/Projects/baremetallm/src/BareLLMServer.dsc \
-      -m /home/crankshaft/Projects/baremetallm/src/Application/BareLLMServer/BareLLMServer.inf \
-      -a X64 \
-      -t GCC
+    -p "$SRC/BareLLMServer.dsc" \
+    -m "$SRC/Application/BareLLMServer/BareLLMServer.inf" \
+    -a X64 \
+    -t GCC
 
-cp /usr/share/edk2/x64/OVMF_VARS.4m.fd /tmp/OVMF_VARS.4m.fd
+# --------------------------------------------------
+# Prepare EFI System Partition
+# --------------------------------------------------
 
-mkdir -p /tmp/esp/EFI/BOOT/
-cp Build/BareLLMServer/DEBUG_GCC/X64/BareLLMServer.efi /tmp/esp/EFI/BOOT/BOOTX64.EFI
+rm -rf "$ESP"
+mkdir -p "$ESP/EFI/BOOT"
+
+cp "$BUILD_DIR/BareLLMServer.efi" \
+   "$ESP/EFI/BOOT/BOOTX64.EFI"
+
+# --------------------------------------------------
+# Copy fresh UEFI variable store
+# --------------------------------------------------
+
+cp "$OVMF_VARS" "$VARS"
+
+# --------------------------------------------------
+# Run QEMU
+# --------------------------------------------------
 
 qemu-system-x86_64 \
-          -machine q35 \
-          -m 512M \
-          -drive if=pflash,format=raw,readonly=on,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd \
-          -drive if=pflash,format=raw,file=/tmp/OVMF_VARS.4m.fd \
-          -drive format=raw,file=fat:rw:/tmp/esp
+    -machine q35 \
+    -m 512M \
+    -drive if=pflash,format=raw,readonly=on,file="$OVMF_CODE" \
+    -drive if=pflash,format=raw,file="$VARS" \
+    -drive format=raw,file=fat:rw:"$ESP"
